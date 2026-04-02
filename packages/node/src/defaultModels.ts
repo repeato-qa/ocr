44c6189357706abad6fb53b9400579f6917493a8
Stephan Petzl <stephan.petzl@gmail.com>
2026-04-02 19:33:02 +0200

    Prepare 1.5.4 release

--- packages/node/src/defaultModels.ts
+++ packages/node/src/defaultModels.ts
@@ -2,16 +2,20 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-const rootDir = path.dirname(fileURLToPath(import.meta.url))
-const assetDir = resolveAssetDir()
-
 export default {
-  detectionPath: path.join(assetDir, 'ch_PP-OCRv4_det_infer.onnx'),
-  recognitionPath: path.join(assetDir, 'ch_PP-OCRv4_rec_infer.onnx'),
-  dictionaryPath: path.join(assetDir, 'ppocr_keys_v1.txt'),
+  get detectionPath() {
+    return path.join(resolveAssetDir(), 'ch_PP-OCRv4_det_infer.onnx')
+  },
+  get recognitionPath() {
+    return path.join(resolveAssetDir(), 'ch_PP-OCRv4_rec_infer.onnx')
+  },
+  get dictionaryPath() {
+    return path.join(resolveAssetDir(), 'ppocr_keys_v1.txt')
+  },
 }
 
 function resolveAssetDir() {
+  const rootDir = resolveRootDir()
   const candidates = [
     path.resolve(rootDir, '../../models/assets'),
     path.resolve(rootDir, '../assets'),
@@ -24,4 +28,12 @@ function resolveAssetDir() {
   }
 
   throw new Error(`Could not find OCR model assets from ${rootDir}`)
+}
+
+function resolveRootDir() {
+  if (typeof __dirname === 'string') {
+    return __dirname
+  }
+
+  return path.dirname(fileURLToPath(import.meta.url))
 }
